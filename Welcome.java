16a1b6f141def1cc3dceaa229a19932c70960d10
ashutosh-singh <ashutosh.01vns@gmail.com>
2023-07-12 10:54:59 +0530

    changed

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
     public static void main(String args[]){
-        System.out.println("Welcome to Java from Jenkins git change-3");
+        System.out.println("Welcome to Java from Jenkins git change from local push");
     }
 }
